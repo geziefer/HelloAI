1e0629011cd1d8f668df9a56c91e96e480c8d492
Alexander Ruehl <alexander.ruehl@sti.valantic.com>
2025-01-27 10:39:31 +0100

    move request mapping to have same structure than in quarkus example

--- hello-springboot/src/main/java/de/vsti/aiduke/hello/service/HelloWorldService.java
+++ hello-springboot/src/main/java/de/vsti/aiduke/hello/service/HelloWorldService.java
@@ -2,16 +2,18 @@ package de.vsti.aiduke.hello.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello-world")
 public class HelloWorldService {
 
     @Autowired
     HelloWorldAssistant assistant;
 
-    @GetMapping("/hello-world")
+    @GetMapping
     public String index(@RequestParam(value = "name", required = false) String name) {
         if (name != null) {
             return assistant.chat("Hello from " + name);
